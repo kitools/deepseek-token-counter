54605841fe044d507cca1e60b8d6fe84fc9e6594
Roomcar <noteforziio@163.com>
2025-02-27 04:53:20 +0800

    Add Hugging Face Transformers dependency and tokenizer implementation

--- src/views/appViews/AppToolView.ts
+++ src/views/appViews/AppToolView.ts
@@ -4,9 +4,14 @@
 
 import {
   h as vnd, defineComponent,
+  reactive,
 } from 'vue';
 
-// import Panel from 'primevue/panel';
+import { deepseek_tokenizer_encode } from '@utils/functions';
+
+import Panel from 'primevue/panel';
+import Textarea from 'primevue/textarea';
+import ToolButton from '@components/shared/ToolButton';
 
 // import { useToast } from 'primevue/usetoast';
 
@@ -17,9 +22,50 @@ const AppToolView = defineComponent({
 
     // const toast = useToast();
 
+    // /** data **/ //
+    const demoData = reactive({
+
+      input: "",
+
+      processing: false,
+      output: [] as number[],
+
+    });
+
     return ()=>{
       return vnd("div", {
-      }, "Hello, World!");
+      }, [
+
+        vnd(Panel, { header: "输入分析", toggleable: true, class: "my-1.5rem! col" }, {
+          default: () => vnd("div", {class: "stack-v"}, [
+            vnd(Textarea, { class: "w-full", placeholder: "输入",
+              modelValue: demoData.input,
+              "onUpdate:modelValue": (value: string) => {
+                demoData.input = value;
+              },
+              disabled: demoData.processing,
+            }),
+
+            vnd("div", {class: "stack-h"}, [
+              vnd(ToolButton, { label: "分析", icon: "pi pi-play", class: "mr-1.5rem",
+                onClick: async () => {
+                  await deepseek_tokenizer_encode(demoData.input).then((res) => {
+                    demoData.output = res;
+                  }
+                  ).catch((err) => {
+                    console.error(err);
+                  }
+                  );
+                },
+              }),
+            ]),
+
+            vnd("pre", { class: "w-full max-h-50vh p-panel p-1rem overflow-auto" }, demoData.output),
+
+            vnd("div", {class: "stack-h"}, []),
+          ]),
+        }),
+      ]);
     };
   }
 })
